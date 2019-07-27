1cf5971bdae9c23e85cfef40646f61adba3652da
Trista Chen <tristachentw@gmail.com>
2019-07-27 16:08:46 +0800

    Updates

---
+++ precache-manifest.cd3203846a9a7d46d79a4551c31a74ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d0877f8c42e62cb7036c5609d8502f",
+    "url": "/freecell-by-react/index.html"
+  },
+  {
+    "revision": "fbf2f1b2d9ea222d7504",
+    "url": "/freecell-by-react/static/css/main.4575ae16.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/freecell-by-react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "fbf2f1b2d9ea222d7504",
+    "url": "/freecell-by-react/static/js/main.3fb56410.chunk.js"
+  },
+  {
+    "revision": "c451e384de49d4ada744",
+    "url": "/freecell-by-react/static/js/runtime~main.f104fc16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/freecell-by-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
