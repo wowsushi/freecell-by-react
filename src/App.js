34d7df6c411a51ca240184571b27c77e98181d3b
Trista Chen <tristachentw@gmail.com>
2019-07-29 22:16:32 +0800

    Complete basic drag and drop function

--- src/App.js
+++ src/App.js
@@ -5,22 +5,32 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state= {
-      mainTableColumns: [],
+      mainTableColumns: [ [], [], [], [], [], [], [], [] ],
+      tempDecks: [ ['heart_10', 'heart_9'], [], [], [] ],
+      sortedDecks: [ [], [], [], [] ],
+      draggedArea: '',
+      draggedColIndex: 0,
+      droppedArea: '',
+      droppedColIndex: 0,
       count: 0
     }
     this.handleClick = this.handleClick.bind(this)
+    this.handleDragStart = this.handleDragStart.bind(this)
+    // this.handleDrop = this.handleDrop.bind(this)
+    this.handleDragEnter = this.handleDragEnter.bind(this)
+    this.handleDragOver = this.handleDragOver.bind(this)
   }
 
   componentDidMount() {
     this.shuffleCards()
-
   }
 
   componentDidUpdate(prevProps) {
-          console.log(this.state.count)
+    console.log(this.state.count)
     console.log(this.state.mainTableColumns)
   }
 
+  //init game
   getRandomNumber(x) {
     return Math.floor(Math.random() * x )
   }
@@ -54,29 +64,136 @@ class App extends Component {
     this.setState({mainTableColumns: mainCardArea })
   }
 
-  draggableCheck(cardList) {
-    return "123"
+  revelCardDecks(cardDecks, status) {
+    const cardList = JSON.parse(JSON.stringify(cardDecks))
+
+    cardList.map((column, columnIndex) => {
+      column.map((cardColor, cardIndex) => {
+        let draggable = (column.length - 1 === cardIndex) ? true : false
+      let cardDiv =
+      <div
+        id={cardColor}
+        key={cardColor}
+        className={'card card_' + status}
+        draggable={draggable}
+        data-columnIndex={columnIndex}>
+          <img
+            src={'cards/' + cardColor + '.png'}
+            alt={cardColor}
+          />
+      </div>
+        return cardList[columnIndex][cardIndex] = cardDiv
+      })
+      cardList[columnIndex] =
+        <div
+          className="card_wrapper"
+          // onDrop={this.handleDrop.bind(this, status)}
+          onDragStart={this.handleDragStart(status, columnIndex)}
+          onDrop={this.handleDrop(status, columnIndex)}
+          onDragEnter={this.handleDragEnter}
+          onDragOver={this.handleDragOver}
+          >
+          { cardList[columnIndex] }
+        </div>
+    })
+    return cardList
   }
 
+  // drag & drop check
+  draggableCheck(cardList) {
+
+  }
+
+  handleDragStart = (status, columnIndex) => e => {
+    console.log('start')
+    e.dataTransfer.setData('text/plain', e.target.parentNode.id)
+    this.setState({draggedArea: status, draggedColIndex: columnIndex})
+  }
+
+  // handleDrop = function (status) {
+  //   return e => {
+  //     .....
+  //   }
+  // }
+
+  handleDrop = (status, columnIndex) => e => {
+    console.log('drop start')
+    const { mainTableColumns, tempDecks, sortedDecks, draggedArea, draggedColIndex, droppedArea } = this.state
+
+    let newMainTableColumns =JSON.parse(JSON.stringify(mainTableColumns))
+    let newTempDecks =JSON.parse(JSON.stringify(tempDecks))
+    let newSortedDecks = JSON.parse(JSON.stringify(sortedDecks))
+
+    this.cancelDefault(e)
+    let id = e.dataTransfer.getData('text/plain')
+
+    //drop card to dropped area
+    switch(status) {
+      case 'temp':
+        newTempDecks[columnIndex].push(id)
+        break;
+      case 'sorted':
+        newSortedDecks[columnIndex].push(id)
+        break;
+      case 'main':
+        console.log(id)
+        newMainTableColumns[columnIndex].push(id)
+        break;
+      default:
+        console.log('default')
+    }
+
+    //remove from dragged area
+    switch(draggedArea) {
+      case 'temp':
+        newTempDecks[draggedColIndex].pop()
+        break;
+      case 'sorted':
+        break;
+      case 'main':
+        newMainTableColumns[draggedColIndex].pop()
+      break;
+      default:
+        console.log('default')
+    }
+
+    this.setState({
+      droppedArea: status,
+      droppedColIndex: columnIndex,
+      mainTableColumns: newMainTableColumns,
+      tempDecks: newTempDecks,
+      sortedDecks: newSortedDecks
+    })
+  }
+
+  handleDragEnter(e) {
+    this.cancelDefault(e)
+  }
+
+  handleDragOver(e) {
+    this.cancelDefault(e)
+  }
+
+  cancelDefault(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    return false
+  }
+
+
+
   handleClick() {
-    this.setState({count: this.state.count +1})
+    this.setState({count: this.state.count + 1})
 
   }
 
   render() {
-    const mainTableColumns = JSON.parse(JSON.stringify(this.state.mainTableColumns))
-
-    mainTableColumns.map((column, columnIndex) => {
-      column.map((cardColor, cardIndex) => {
-        let draggable = (column.length - 1 === cardIndex) ? true : false
-      let cardDiv = <div id={cardColor} class="card card_main" draggable={draggable}><img src={'cards/' + cardColor + '.png'} alt={cardColor}/></div>
-        mainTableColumns[columnIndex][cardIndex] = cardDiv
-      })
-      mainTableColumns[columnIndex] = <div className="card_wrapper">{ mainTableColumns[columnIndex] }</div>
-    })
+    const mainTableColumns = this.revelCardDecks(this.state.mainTableColumns, 'main')
+    const tempDecks = this.revelCardDecks(this.state.tempDecks, 'temp')
+    const sortedDecks = this.revelCardDecks(this.state.sortedDecks, 'sorted')
 
     return (
-    <div className="container" onClick={this.handleClick}>
+    <div className="container" >
         <div className="header">
           <img className="main_banner" src="logo-bk.png" alt="logo" />
           <div className="nav">
@@ -91,13 +208,11 @@ class App extends Component {
         </div>
         <div className="game_table">
           <div className="cards_area cards_area_temp">
-            <div className="card_wrapper"></div>
-            <div className="card_wrapper"></div>
-            <div className="card_wrapper"></div>
-            <div className="card_wrapper"></div>
+            {tempDecks}
           </div>
           <div className="cards_area cards_area_sorted">
-            <div className="card_wrapper">
+            {sortedDecks}
+            {/* <div className="card_wrapper">
               <img src="club-suit-w.png" alt="club"/>
             </div>
             <div className="card_wrapper">
@@ -108,7 +223,7 @@ class App extends Component {
             </div>
             <div className="card_wrapper">
               <img src="spade-suit-w.png" alt="spade"/>
-            </div>
+            </div> */}
           </div>
           <div className="cards_area cards_area_main left">
             {mainTableColumns.map((column, index) => {
